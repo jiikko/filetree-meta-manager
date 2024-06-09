7441886ae199670793d1d82343b3b6d55b3448db
jiikko <n905i.1214@gmail.com>
2024-06-09 19:51:24 +0900

    ファイルの種類によってアイコンを変化させる

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,8 @@ module ApplicationHelper
     content_tag(:ul) do
       node['children'].map do |child|
         has_children = child['children'].present?
-        class_name = has_children ? 'directory' : nil
+        class_names = has_children ? ['directory'] : []
+        class_names << 'file_movie' if child['path'].match?(/(mp4|mkv|avi|mov|flv|wmv|mpg|mpeg)$/)
         if has_children
           row_data_attr = {
             controller: 'clipboard',
@@ -12,7 +13,7 @@ module ApplicationHelper
           }
         end
 
-        content_tag(:li, class: class_name, data: row_data_attr) do
+        content_tag(:li, class: class_names.join(' '), data: row_data_attr) do
           concat(child['path'])
           concat(render_filetree(child)) if has_children
         end
