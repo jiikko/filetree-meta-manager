dc40946cf9e7dcaeaae4bcf0e32894c50ae81aaa
jiikko <n905i.1214@gmail.com>
2024-06-16 15:39:21 +0900

    Use Rack::Deflater

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ Bundler.require(*Rails.groups)
 
 module FiletreeMetaManager
   class Application < Rails::Application
+    config.middleware.use Rack::Deflater
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
